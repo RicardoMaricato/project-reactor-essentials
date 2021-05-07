51bc609ad9ef438ae8a39b5d4a436fa910f8cd7d
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-06 22:08:51 -0300

    -MonoSubscriberConsumerComplete -MonoSubscriberConsumerSubscription

--- src/test/java/com/ricardo/reactive/test/MonoTest.java
+++ src/test/java/com/ricardo/reactive/test/MonoTest.java
@@ -2,6 +2,7 @@ package com.ricardo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -64,5 +65,42 @@ public class MonoTest {
         StepVerifier.create(mono)
                 .expectError(RuntimeException.class)
                 .verify();
+    }
+
+    @Test
+    public void monoSubscriberConsumerComplete() {
+        String name = "Ricardo Maricato";
+        Mono<String> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("FINISHED!"));
+
+        log.info("--------------------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoSubscriberConsumerSubscription() {
+        String name = "Ricardo Maricato";
+        Mono<String> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("FINISHED!")
+                , Subscription::cancel);
+
+        log.info("--------------------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
     }
 }
