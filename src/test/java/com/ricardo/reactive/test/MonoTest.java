46ee51803a97cd5f404d82800e09d2c4b951837d
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-05-06 21:49:14 -0300

    -MonoSubscriber

--- src/test/java/com/ricardo/reactive/test/MonoTest.java
+++ src/test/java/com/ricardo/reactive/test/MonoTest.java
@@ -2,6 +2,8 @@ package com.ricardo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 /**
@@ -22,7 +24,15 @@ import org.junit.jupiter.api.Test;
 public class MonoTest {
 
     @Test
-    public void test() {
-        log.info("Everthing working as intended");
+    public void monoSubscriber() {
+        String name = "Ricardo Maricato";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe();
+        log.info("--------------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
     }
 }
